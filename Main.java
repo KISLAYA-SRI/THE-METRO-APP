c2fa0d4791aa64fb8a6f0087f0989bc4ce0e226e
anushkasinghal11 <you@example.com>
2020-12-07 12:51:33 +0530

    explained override compareTo

--- Main.java
+++ Main.java
@@ -159,6 +159,21 @@ import java.io.*;
 			String psf;
 			int cost;
 
+			/*
+			The compareTo method is defined in Java.land.Comparable.
+			Here we override the method because the conventional compareTo method
+			is used to compare strings,integers and other primitive data types. But
+			here in this case, we intend to compare two objects of DijkstraPair class.
+			*/ 
+
+			/*
+			Removing the overriden method gives us this errror:
+			The type Graph_M.DijkstraPair must implement the inherited abstract method Comparable<Graph_M.DijkstraPair>.compareTo(Graph_M.DijkstraPair)
+
+			This is because DijkstraPair is not an abstract class and implements Comparable interface which has an abstract 
+			method compareTo. In order to make our class concrete(a class which provides implementation for all its methods)
+			we have to override the method compareTo
+			 */
 			@Override
 			public int compareTo(DijkstraPair o) 
 			{
