e7bd847850401284434c13b80d351d8ccbbdb217
suryam35 <suryamkalra35@gmail.com>
2020-12-10 06:45:42 -0500

    Made it more user-friendly

--- Graph_M.java
+++ Graph_M.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 	
 	public class Graph_M 
 	{
@@ -99,7 +100,7 @@ import java.io.*;
 
 		public void display_Map() 
 		{
-			System.out.println("------------------");
+			System.out.println("----------------------------------------------------\n");
 			ArrayList<String> keys = new ArrayList<>(vtces.keySet());
 
 			for (String key : keys) 
@@ -109,13 +110,13 @@ import java.io.*;
 				str = str + vtx.nbrs;
 				System.out.println(str);
 			}
-			System.out.println("------------------");
+			System.out.println("---------------------------------------------------\n");
 
 		}
 		
 		public void display_Stations() 
 		{
-			System.out.println("....................................");
+			System.out.println("\n***********************************************************************\n");
 			ArrayList<String> keys = new ArrayList<>(vtces.keySet());
 			int i=1;
 			for(String key : keys) 
@@ -123,7 +124,7 @@ import java.io.*;
 				System.out.println(i + ". " + key);
 				i++;
 			}
-			System.out.println("....................................");
+			System.out.println("\n***********************************************************************\n");
 		}
 			
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -469,111 +470,131 @@ import java.io.*;
 			Graph_M g = new Graph_M();
 			Create_Metro_Map(g);
 			
-			System.out.println("****WELCOME TO THE METRO APP*****");
-			System.out.println("      ~~MENU OPTIONS~~");
-			System.out.println("1. LIST ALL THE STATIONS IN THE MAP");
-			System.out.println("2. SHOW THE METRO MAP");
-			System.out.println("3. GET SHORTEST DISTANCE FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
-			System.out.println("4. GET SHORTEST TIME TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
-			System.out.println("5. GET SHORTEST PATH (DISTANCE WISE) TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
-			System.out.println("6. GET SHORTEST PATH (TIME WISE) TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
-			System.out.println("ENTER YOUR CHOICE");
+			System.out.println("\n\t\t\t****WELCOME TO THE METRO APP*****");
+			// System.out.println("\t\t\t\t~~LIST OF ACTIONS~~\n\n");
+			// System.out.println("1. LIST ALL THE STATIONS IN THE MAP");
+			// System.out.println("2. SHOW THE METRO MAP");
+			// System.out.println("3. GET SHORTEST DISTANCE FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+			// System.out.println("4. GET SHORTEST TIME TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+			// System.out.println("5. GET SHORTEST PATH (DISTANCE WISE) TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+			// System.out.println("6. GET SHORTEST PATH (TIME WISE) TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+			// System.out.print("\nENTER YOUR CHOICE FROM THE ABOVE LIST : ");
 			BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
-			int choice = Integer.parseInt(inp.readLine());
+			// int choice = Integer.parseInt(inp.readLine());
 			//STARTING SWITCH CASE
-			switch(choice)
+			while(true)
 			{
-			case 1:
-				g.display_Stations();
-				g.display_Map();
-				break;
+				System.out.println("\t\t\t\t~~LIST OF ACTIONS~~\n\n");
+				System.out.println("1. LIST ALL THE STATIONS IN THE MAP");
+				System.out.println("2. SHOW THE METRO MAP");
+				System.out.println("3. GET SHORTEST DISTANCE FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+				System.out.println("4. GET SHORTEST TIME TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+				System.out.println("5. GET SHORTEST PATH (DISTANCE WISE) TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+				System.out.println("6. GET SHORTEST PATH (TIME WISE) TO REACH FROM A 'SOURCE' STATION TO 'DESTINATION' STATION");
+				System.out.println("7. EXIT THE MENU");
+				System.out.print("\nENTER YOUR CHOICE FROM THE ABOVE LIST (1 to 7) : ");
+				int choice = Integer.parseInt(inp.readLine());
+				System.out.print("\n***********************************************************\n");
+				if(choice == 7)
+				{
+					System.exit(0);
+				}
+				switch(choice)
+				{
+				case 1:
+					g.display_Stations();
+					break;
 			
-			case 2:
-				g.display_Map();
-				break;
+				case 2:
+					g.display_Map();
+					break;
 				
-			case 3:
-				System.out.println("ENTER THE SOURCE AND DESTINATION STATIONS");
-				String st1 = inp.readLine();
-				String st2 = inp.readLine();
+				case 3:
+					System.out.println("ENTER THE SOURCE AND DESTINATION STATIONS");
+					String st1 = inp.readLine();
+					String st2 = inp.readLine();
 				
-				HashMap<String, Boolean> processed = new HashMap<>();
-				if(!g.containsVertex(st1) || !g.containsVertex(st2) || !g.hasPath(st1, st2, processed))
-					System.out.println("THE INPUTS ARE INVALID");
-				else
-				System.out.println("SHORTEST DISTANCE FROM "+st1+" TO "+st2+" IS "+g.dijkstra(st1, st2, false)+"KM");
-				break;
+					HashMap<String, Boolean> processed = new HashMap<>();
+					if(!g.containsVertex(st1) || !g.containsVertex(st2) || !g.hasPath(st1, st2, processed))
+						System.out.println("THE INPUTS ARE INVALID");
+					else
+					System.out.println("SHORTEST DISTANCE FROM "+st1+" TO "+st2+" IS "+g.dijkstra(st1, st2, false)+"KM");
+					break;
 				
-			case 4:
-				System.out.println("ENTER THE SOURCE AND DESTINATION STATIONS");
-				String sat1 = inp.readLine();
-				String sat2 = inp.readLine();
+				case 4:
+					System.out.print("ENTER THE SOURCE STATION: ");
+					String sat1 = inp.readLine();
+					System.out.print("ENTER THE DESTINATION STATION: ");
+					String sat2 = inp.readLine();
 				
-				HashMap<String, Boolean> processed1= new HashMap<>();				
-				System.out.println("SHORTEST DISTANCE FROM "+sat1+" TO "+sat2+" IS "+g.dijkstra(sat1, sat2, true)/60+"KM");
-				break;
+					HashMap<String, Boolean> processed1= new HashMap<>();				
+					System.out.println("SHORTEST TIME FROM ("+sat1+") TO ("+sat2+") IS "+g.dijkstra(sat1, sat2, true)/60+" MINUTES\n\n");
+					break;
 				
-			case 5:
-				System.out.println("ENTER THE SOURCE AND DESTINATION STATIONS");
-				String s1 = inp.readLine();
-				String s2 = inp.readLine();
+				case 5:
+					System.out.println("ENTER THE SOURCE AND DESTINATION STATIONS");
+					String s1 = inp.readLine();
+					String s2 = inp.readLine();
 				
-				HashMap<String, Boolean> processed2 = new HashMap<>();
-				if(!g.containsVertex(s1) || !g.containsVertex(s2) || !g.hasPath(s1, s2, processed2))
-					System.out.println("THE INPUTS ARE INVALID");
-				else 
-				{
-					ArrayList<String> str = g.get_Interchanges(g.Get_Minimum_Distance(s1, s2));
-					int len = str.size();
-					System.out.println("SOURCE STATION : " + s1);
-					System.out.println("SOURCE STATION : " + s2);
-					System.out.println("DISTANCE : " + str.get(len-1));
-					System.out.println("NUMBER OF INTERCHANGES : " + str.get(len-2));
-					//System.out.println(str);
-					System.out.println("~~~~~~~~~~~~~");
-					System.out.println("START  ==>  " + str.get(0));
-					for(int i=1; i<len-3; i++)
+					HashMap<String, Boolean> processed2 = new HashMap<>();
+					if(!g.containsVertex(s1) || !g.containsVertex(s2) || !g.hasPath(s1, s2, processed2))
+						System.out.println("THE INPUTS ARE INVALID");
+					else 
 					{
-						System.out.println(str.get(i));
+						ArrayList<String> str = g.get_Interchanges(g.Get_Minimum_Distance(s1, s2));
+						int len = str.size();
+						System.out.println("SOURCE STATION : " + s1);
+						System.out.println("SOURCE STATION : " + s2);
+						System.out.println("DISTANCE : " + str.get(len-1));
+						System.out.println("NUMBER OF INTERCHANGES : " + str.get(len-2));
+						//System.out.println(str);
+						System.out.println("~~~~~~~~~~~~~");
+						System.out.println("START  ==>  " + str.get(0));
+						for(int i=1; i<len-3; i++)
+						{
+							System.out.println(str.get(i));
+						}
+						System.out.print(str.get(len-3) + "   ==>    END");
+						System.out.println("\n~~~~~~~~~~~~~");
 					}
-					System.out.print(str.get(len-3) + "   ==>    END");
-					System.out.println("\n~~~~~~~~~~~~~");
-				}
-				break;
+					break;
 				
-			case 6:
-				System.out.println("ENTER THE SOURCE AND DESTINATION STATIONS");
-				String ss1 = inp.readLine();
-				String ss2 = inp.readLine();
+				case 6:
+					System.out.print("ENTER THE SOURCE STATION: ");
+					String ss1 = inp.readLine();
+					System.out.print("ENTER THE DESTINATION STATION: ");
+					String ss2 = inp.readLine();
 				
-				HashMap<String, Boolean> processed3 = new HashMap<>();
-				if(!g.containsVertex(ss1) || !g.containsVertex(ss2) || !g.hasPath(ss1, ss2, processed3))
-					System.out.println("THE INPUTS ARE INVALID");
-				else
-				{
-					ArrayList<String> str = g.get_Interchanges(g.Get_Minimum_Time(ss1, ss2));
-					int len = str.size();
-					System.out.println("SOURCE STATION : " + ss1);
-					System.out.println("SOURCE STATION : " + ss2);
-					System.out.println("TIME : " + str.get(len-1)+" MINUTES");
-					System.out.println("NUMBER OF INTERCHANGES : " + str.get(len-2));
-					//System.out.println(str);
-					System.out.println("~~~~~~~~~~~~~");
-					System.out.println("START  ==>  " + str.get(0));
-					for(int i=1; i<len-3; i++)
+					HashMap<String, Boolean> processed3 = new HashMap<>();
+					if(!g.containsVertex(ss1) || !g.containsVertex(ss2) || !g.hasPath(ss1, ss2, processed3))
+						System.out.println("THE INPUTS ARE INVALID");
+					else
 					{
-						System.out.println(str.get(i));
+						ArrayList<String> str = g.get_Interchanges(g.Get_Minimum_Time(ss1, ss2));
+						int len = str.size();
+						System.out.println("SOURCE STATION : " + ss1);
+						System.out.println("DESTINATION STATION : " + ss2);
+						System.out.println("TIME : " + str.get(len-1)+" MINUTES");
+						System.out.println("NUMBER OF INTERCHANGES : " + str.get(len-2));
+						//System.out.println(str);
+						System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+						System.out.print("START  ==>  " + str.get(0) + " ==>  ");
+						for(int i=1; i<len-3; i++)
+						{
+							System.out.println(str.get(i));
+						}
+						System.out.print(str.get(len-3) + "   ==>    END");
+						System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 					}
-					System.out.print(str.get(len-3) + "   ==>    END");
-					System.out.println("\n~~~~~~~~~~~~~");
-				}
-				break;	
-                        default:  //If switch expression does not match with any case, 
-                        //default statements are executed by the program.
-                            //No break is needed in the default case
-                            System.out.println("Please enter a valid option! ");
-                            System.out.println("The options you can choose are from 1 to 6. ");
+					break;	
+               	         default:  //If switch expression does not match with any case, 
+                	        	//default statements are executed by the program.
+                            	//No break is needed in the default case
+                    	        System.out.println("Please enter a valid option! ");
+                        	    System.out.println("The options you can choose are from 1 to 6. ");
                             
+				}
 			}
+			
 		}	
 	}
